b136f14d8f8f789c9faf8a3e6c1d3df014b2ee09
Hissen Tutu <hissentutu@gmail.comm>
2026-08-21 14:44:11 +0300

    wrote three functions ,squere,is_even and celsisu_to_fahremheit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from utils import square,is_even,celsius_to_fahrenheit
+
+
+
+
+print("Enter number:.....")
+
+number=float(input())
+
+print("There square of the number is:",square(number))
---
+++ src/utils.py
@@ -0,0 +1,12 @@
+def square(n):
+    return n*n
+
+def is_even(n):
+    if(n%2==0):
+        return True
+    return False
+def celsius_to_fahrenheit(c):
+    F=(c*float(9/5)+32)
+    return F
+
+    
